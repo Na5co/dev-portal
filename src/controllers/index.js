a509e8b6e8b70e957e966a1281875cb233784712
Atanas Ameti <atanas1899@gmail.com>
2025-09-22 03:03:43 +0100

    redeploy route

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,8 @@
-const {
-  authController,
-  userController,
-  bankingController,
-  devController,
-  adminController,
-} = require('../controllers');
+const authController = require('./auth.controller');
+const userController = require('./user.controller');
+const bankingController = require('./banking.controller');
+const devController = require('./dev.controller');
+const adminController = require('./admin.controller');
 
 module.exports = {
   authController,
--- src/validations/index.js
+++ src/validations/index.js
@@ -1,10 +1,11 @@
-module.exports.customValidation = require('./custom.validation');
+const customValidation = require('./custom.validation');
 const authValidation = require('./auth.validation');
 const userValidation = require('./user.validation');
 const bankingValidation = require('./banking.validation');
 const adminValidation = require('./admin.validation');
 
 module.exports = {
+  customValidation,
   authValidation,
   userValidation,
   bankingValidation,
